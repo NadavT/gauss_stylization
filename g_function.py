ecdf24480b11f920e1ec50a2b0c9b29eba74446b
Nadav Tur <t-nadavtur@microsoft.com>
2023-02-08 13:16:59 +0200

    Vectorized code

--- g_function.py
+++ g_function.py
@@ -5,13 +5,13 @@ from math import exp
 
 class g_Function:
     def __init__(self, N: list[np.array], R: list[int], R_axis: np.array, sigma: float, mu: float, lambda_value: float, caxiscontrib: float):
-        self.N = N
-        self.R = R
+        self.N = np.array(N)
+        self.R = np.array(R)
         self.R_axis = R_axis
         self.sigma = sigma
         self.mu = mu
         self.lambda_value = lambda_value
-        self.caxiscontrib = caxiscontrib
+        self.caxiscontrib = caxiscontrib if len(R) > 0 else 1
         self._normalize_g_weights()
 
     def _normalize_g_weights(self):
@@ -25,22 +25,22 @@ class g_Function:
             self.R_w = sp.linalg.solve(mat, np.ones(len(self.R)))
 
     def value(self, v: np.array):
-        return self.caxiscontrib * sum(self.N_w[i] * exp(self.sigma * v.dot(self.N[i])) for i in range(len(self.N))) + \
-            sum(self.R_w[i] * exp(self.sigma * (1 - (v.dot(self.R_axis) - self.R[i])**2))
-                for i in range(len(self.R)))
+        N_value = np.sum(self.N_w * np.exp(self.sigma *
+                         v.dot(self.N.transpose())))
+        R_value = np.sum(self.R_w * np.exp(self.sigma * (1 -
+                         (v.dot(self.R_axis) - self.R)**2))) if len(self.R) > 0 else 0
+        return (self.caxiscontrib * N_value + R_value)
 
     def gradient(self, v: np.array):
-        return self.sigma * (self.caxiscontrib * sum(self.N_w[i] * self.N[i] * exp(self.sigma * v.dot(self.N[i])) for i in range(len(self.N))) +
-                             sum(self.R_w[i] * self.R_axis * -2 * (v.dot(self.R_axis) - self.R[i]) * exp(self.sigma * (1 - (v.dot(self.R_axis) - self.R[i])**2)) for i in range(len(self.R))))
+        N_grad = np.sum(self.N_w.reshape(-1, 1) * self.N *
+                        np.exp(self.sigma * v.dot(self.N.transpose())).reshape((-1, 1)), axis=0)
+        R_grad = np.sum(self.R_w * np.repeat(self.R_axis.reshape(-1, 1), 2, axis=1) * -2 * (v.dot(self.R_axis) - self.R)
+                        * np.exp(self.sigma * (1 - np.power(v.dot(self.R_axis) - self.R, 2))), axis=1) if len(self.R) > 0 else 0
+        return self.sigma * (self.caxiscontrib * N_grad + R_grad)
 
     def hessian(self, v: np.array):
-        return (
-            (self.sigma**2) * sum(
-                self.N_w[i] * exp(self.sigma * v.dot(self.N[i])) *
-                self.N[i].reshape((-1, 1)) * self.N[i]
-                for i in range(len(self.N))) +
-            sum(
-                self.R_w[i] * 2 * self.sigma * (2 * self.sigma * (v.dot(self.R_axis) - self.R[i])**2 - 1) * exp(
-                    self.sigma * (1 - (v.dot(self.R_axis) - self.R[i])**2)) * self.R_axis.reshape((-1, 1)) * self.R_axis
-                for i in range(len(self.R))
-            ))
+        N_hess = (self.sigma**2) * np.sum(self.N_w.reshape(-1, 1) * np.exp(self.sigma *
+                                                                           v.dot(self.N.transpose())).reshape((-1, 1))) * self.N.transpose().dot(self.N)
+        R_hess = np.sum(self.R_w * 2 * self.sigma * (2 * self.sigma * np.power((v.dot(self.R_axis) - self.R), 2) - 1) * np.exp(
+            self.sigma * (1 - np.power((v.dot(self.R_axis) - self.R), 2)))) * self.R_axis.reshape((-1, 1)) * self.R_axis
+        return (self.caxiscontrib * N_hess + R_hess)
