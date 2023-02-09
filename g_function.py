7590ea699f882636fb721524e7396736f2210418
Nadav Tur <t-nadavtur@microsoft.com>
2023-02-09 11:14:23 +0200

    Better vectorization

--- g_function.py
+++ g_function.py
@@ -5,9 +5,9 @@ from math import exp
 
 class g_Function:
     def __init__(self, N: list[np.array], R: list[int], R_axis: list[np.array], sigma: float, mu: float, lambda_value: float, caxiscontrib: float):
-        self.N = np.array(N)
+        self.N = np.array(N).transpose()
         self.R = np.array(R)
-        self.R_axis = np.array(R_axis)
+        self.R_axis = np.array(R_axis).transpose()
         self.sigma = sigma
         self.mu = mu
         self.lambda_value = lambda_value
@@ -16,41 +16,30 @@ class g_Function:
 
     def _normalize_g_weights(self):
         if len(self.N) > 0:
-            mat = np.array([[exp(self.sigma * self.N[i][:].dot(self.N[j][:]))
-                             for j in range(len(self.N))] for i in range(len(self.N))])
-            self.N_w = sp.linalg.solve(mat, np.ones(len(self.N)))
+            mat = np.exp(self.sigma * self.N.transpose().dot(self.N))
+            self.N_w = sp.linalg.solve(mat, np.ones(self.N.shape[1]))
         if len(self.R) > 0:
-            mat = np.array([[exp(self.sigma * (1 - (self.R[i] - self.R[j])**2))
-                             for j in range(len(self.R))] for i in range(len(self.R))])
+            mat = np.exp(
+                self.sigma * (1 - (self.R - self.R.reshape(-1, 1))**2))
             self.R_w = sp.linalg.solve(mat, np.ones(len(self.R)))
 
     def value(self, v: np.array):
         N_value = np.sum(self.N_w * np.exp(self.sigma *
-                         v.dot(self.N.transpose())))
+                         v.dot(self.N)))
         R_value = np.sum(self.R_w * np.exp(self.sigma * (1 -
-                         (v.dot(self.R_axis.transpose()) - self.R)**2))) if len(self.R) > 0 else 0
+                         (v.dot(self.R_axis) - self.R)**2))) if len(self.R) > 0 else 0
         return (self.caxiscontrib * N_value + R_value)
 
     def gradient(self, v: np.array):
-        N_grad = np.sum(self.N_w.reshape(-1, 1) * self.N *
-                        np.exp(self.sigma * v.dot(self.N.transpose())).reshape((-1, 1)), axis=0)
-        R_grad = np.sum(self.R_w * self.R_axis.transpose() * -2 * (v.dot(self.R_axis.transpose()) - self.R)
-                        * np.exp(self.sigma * (1 - np.power(v.dot(self.R_axis.transpose()) - self.R, 2))), axis=1) if len(self.R) > 0 else 0
+        N_grad = np.sum(self.N_w.reshape(-1, 1) * self.N.transpose() *
+                        np.exp(self.sigma * v.dot(self.N)).reshape((-1, 1)), axis=0)
+        R_grad = np.sum(self.R_w * self.R_axis * -2 * (v.dot(self.R_axis) - self.R)
+                        * np.exp(self.sigma * (1 - np.power(v.dot(self.R_axis) - self.R, 2))), axis=1) if len(self.R) > 0 else 0
         return self.sigma * (self.caxiscontrib * N_grad + R_grad)
 
     def hessian(self, v: np.array):
-        return (
-            (self.sigma**2) * sum(
-                self.N_w[i] * exp(self.sigma * v.dot(self.N[i])) *
-                self.N[i].reshape((-1, 1)) * self.N[i]
-                for i in range(len(self.N))) +
-            sum(
-                self.R_w[i] * 2 * self.sigma * (2 * self.sigma * (v.dot(self.R_axis) - self.R[i])**2 - 1) * exp(
-                    self.sigma * (1 - (v.dot(self.R_axis) - self.R[i])**2)) * self.R_axis.reshape((-1, 1)) * self.R_axis
-                for i in range(len(self.R))
-            ))
-        # N_hess = (self.sigma**2) * np.sum(self.N_w.reshape(-1, 1) * np.exp(self.sigma *
-        #                                                                    v.dot(self.N.transpose())).reshape((-1, 1))) * self.N.transpose().dot(self.N)
-        # R_hess = np.sum(self.R_w * 2 * self.sigma * (2 * self.sigma * np.power((v.dot(self.R_axis.transpose()) - self.R), 2) - 1) * np.exp(
-        #     self.sigma * (1 - np.power((v.dot(self.R_axis.transpose()) - self.R), 2)))) * self.R_axis.transpose().dot(self.R_axis) if len(self.R) > 0 else 0
-        # return (self.caxiscontrib * N_hess + R_hess)
+        modified_N = np.sqrt(
+            self.N_w * np.exp(self.sigma * v.dot(self.N))) * self.N
+        modified_R = np.sqrt(self.R_w *
+                             np.exp(self.sigma * (1 - (v.dot(self.R_axis) - self.R)**2))) * self.R_axis if len(self.R) > 0 else np.array([0])
+        return (self.sigma**2) * (self.caxiscontrib * modified_N.dot(modified_N.transpose()) + modified_R.dot(modified_R.transpose()))
